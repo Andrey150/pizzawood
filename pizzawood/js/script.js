f1b605c2e068eb9e933ac7c4bad02171961c38ef
Andrey150 <69532060+Andrey150@users.noreply.github.com>
2021-02-15 15:00:32 +0300

    Add files via upload

---
+++ pizzawood/js/script.js
@@ -0,0 +1,26 @@
+
+/*Переключатель*/
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true, 
+        dots: false,
+        adaptiveHeight: true,
+        centerMode: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll:1,
+        infinite: true,
+        initialSlide: 0,
+        
+       
+    
+    });
+    
+});
+
+
+
+
+
+
+
